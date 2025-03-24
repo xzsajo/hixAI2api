1389a41bb3d2d17f10e579f7333ea1daeea15977
dean <1006393151@qq.com>
2025-03-24 15:37:06 +0800

    fix: del chat

--- job/chat.go
+++ job/chat.go
@@ -15,18 +15,15 @@ func DelChatTask() {
 	if config.ChatMaxDays < 0 {
 		return
 	}
-
 	client := cycletls.Init()
 	defer safeClose(client)
 	for {
 		logger.SysLog("hixai2api Scheduled DelChatTask Task Job Start!")
-
 		chat := &model.Chat{}
 		chats, err := chat.FindOlderThan(database.DB, config.ChatMaxDays)
 		if err != nil {
 			logger.SysError(fmt.Sprintf("FindOlderThan err: %v Id: %s", err, chat.Id))
 		}
-
 		for _, chat := range chats {
 			err := chat.DeleteById(database.DB)
 			if err != nil {
@@ -37,8 +34,12 @@ func DelChatTask() {
 				logger.SysError(fmt.Sprintf("MakeDelChatRequest err: %v Id: %s", err, chat.Id))
 			}
 		}
+		logger.SysLog("hixai2api Scheduled DelChatTask Task Job End!")
 
-		logger.SysLog("hixai2api Scheduled DelChatTask Task Job  End!")
-		time.Sleep(60 * time.Minute)
+		// 计算到下一个凌晨0点的时间
+		now := time.Now()
+		next := now.Add(24 * time.Hour)
+		next = time.Date(next.Year(), next.Month(), next.Day(), 0, 0, 0, 0, next.Location())
+		time.Sleep(next.Sub(now))
 	}
 }
